59ec22da63ba2f4acfcbd6afe129e47f87cdcfbe
MistarJo <cbh2962@gmail.com>
2023-11-28 15:09:36 +0900

    [별희] 로그인 실패 시 팝업 안뜨는 버그 수정

--- Source/MetaAuction/UI/Title/MALoginWidget.cpp
+++ Source/MetaAuction/UI/Title/MALoginWidget.cpp
@@ -38,19 +38,22 @@ void UMALoginWidget::NativeConstruct()
 		MAGameInstance->OnLoginDelegate.Remove(OnLoginDelegateHandle);
 		OnLoginDelegateHandle = MAGameInstance->OnLoginDelegate.AddLambda([ThisPtr](bool bIsSuccessed)
 			{
-				// 로그인 성공 시 위젯 파괴
-				if (ThisPtr.IsValid() && bIsSuccessed)
+				if (ThisPtr.IsValid())
 				{
-					ThisPtr->RemoveFromParent();
-				}
-				// 로그인 실패 시 결과 팝업 창 생성
-				else
-				{
-					if (AMAPlayerController* MAPC = Cast<AMAPlayerController>(ThisPtr->GetOwningPlayer()))
+					// 로그인 성공 시 위젯 파괴
+					if (bIsSuccessed)
 					{
-						if (UMAConfirmPopupWidget* PopupWidget = MAPC->CreateAndAddConfirmPopupWidget())
+						ThisPtr->RemoveFromParent();
+					}
+					// 로그인 실패 시 결과 팝업 창 생성
+					else
+					{
+						if (AMAPlayerController* MAPC = Cast<AMAPlayerController>(ThisPtr->GetOwningPlayer()))
 						{
-							PopupWidget->SetText(TEXT("로그인에 실패하였습니다."));
+							if (UMAConfirmPopupWidget* PopupWidget = MAPC->CreateAndAddConfirmPopupWidget())
+							{
+								PopupWidget->SetText(TEXT("로그인에 실패하였습니다."));
+							}
 						}
 					}
 				}
